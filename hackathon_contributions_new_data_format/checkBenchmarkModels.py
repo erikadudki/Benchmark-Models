bca2df55d7787d72cb1b42fc0a47431b5dc54bb3
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-02-16 18:45:06 +0100

    Fix checkBenchmarkModel.py, ignore files

--- hackathon_contributions_new_data_format/checkBenchmarkModels.py
+++ hackathon_contributions_new_data_format/checkBenchmarkModels.py
@@ -2,11 +2,9 @@
 
 import os
 
-model_list = os.listdir()
-model_list = sorted(model_list)
-print(model_list)
+model_list = os.scandir()
+model_list = sorted(f.name  for f in model_list if f.is_dir())
 
 for benchmark_model in model_list:
-    if '.MD' not in benchmark_model and '.py' not in benchmark_model:
-        os.system("cd {} && petablint.py -v -n {} ".format(benchmark_model,benchmark_model))
-        print('='*100) # just for output readability
+    os.system("cd {} && petablint.py -v -n {} ".format(benchmark_model,benchmark_model))
+    print('='*100) # just for output readability
