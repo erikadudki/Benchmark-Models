eb464107af73ec1e00d0eed13f378dfa7a9ad6ca
Erika <edudkin@uni-bonn.de>
2019-04-09 14:55:56 +0200

    added whitespaces, style

--- hackathon_contributions_new_data_format/Isensee_JCB2018/visualization/main.py
+++ hackathon_contributions_new_data_format/Isensee_JCB2018/visualization/main.py
@@ -15,13 +15,14 @@ VisualizationFilePath = "https://raw.githubusercontent.com/LoosC/"\
                         "Benchmark-Models/hackathon_contributions"\
                         "_new_data_format/Isensee_JCB2018/visualizationSpecific"\
                         "ation_Isensee_JCB2018.tsv"
+#test
 
 # import measurement data
-measurement_data= pd.DataFrame.from_csv(
+measurement_data = pd.DataFrame.from_csv(
         DataFilePath, sep="\t", index_col=None)
 
 # import experimental condition
-experimental_condition= pd.DataFrame.from_csv(
+experimental_condition = pd.DataFrame.from_csv(
         ConditionFilePath, sep="\t", index_col=None)
 
 # import visualization specification
@@ -33,19 +34,19 @@ cmap = ['#8c510a','#bf812d','#dfc27d','#f6e8c3','#c7eae5','#80cdc1','#35978f','#
 
 
 # get unique plotIDs
-plotIds, plotInd=np.unique(visualization_specification.plotId, return_index=True)
+plotIds, plotInd = np.unique(visualization_specification.plotId, return_index=True)
 print(plotIds)
 print(plotInd)
 
 for i_plotId, var_plotId in enumerate(plotIds):
     print(i_plotId)
     print(var_plotId)
-    ind_plot = visualization_specification['plotId']==var_plotId
+    ind_plot = visualization_specification['plotId'] == var_plotId
     print(visualization_specification[ind_plot])
     for i in visualization_specification[ind_plot].index.values:
         # get datasetID and independent variable of first entry of plot1
-        datasetId=visualization_specification.datasetId[i]
-        indepVar=visualization_specification.independentVariable[i]
+        datasetId = visualization_specification.datasetId[i]
+        indepVar = visualization_specification.independentVariable[i]
 
         # define index to reduce measurement_data to data linked to datasetId
         ind_dataset = measurement_data['datasetId'] == datasetId
@@ -57,12 +58,12 @@ for i_plotId, var_plotId in enumerate(plotIds):
             print(indepVar)
 
             # gather simulationConditionIds belonging to datasetId
-            conditionIds=np.unique(measurement_data[ind_dataset].simulationConditionId)
+            conditionIds = np.unique(measurement_data[ind_dataset].simulationConditionId)
             print(conditionIds)
 
             # extract conditions (plot input) from condition file
-            ind_cond=experimental_condition['conditionId'].isin(conditionIds)
-            conditions=experimental_condition[ind_cond][indepVar]
+            ind_cond = experimental_condition['conditionId'].isin(conditionIds)
+            conditions = experimental_condition[ind_cond][indepVar]
             print(conditions)
 
             # create empty dataframe for means and SDs
@@ -71,13 +72,13 @@ for i_plotId, var_plotId in enumerate(plotIds):
 
             # group measurement values for each conditionId
             for ID in conditionIds:
-                meas=measurement_data['simulationConditionId']==ID
+                meas = measurement_data['simulationConditionId'] == ID
                 print(ID)
                 print(measurement_data[meas].measurement)
                 print(np.mean(measurement_data[meas].measurement))
                 print(np.std(measurement_data[meas].measurement))
-                ms.at[ID,'mean']= np.mean(measurement_data[meas].measurement)
-                ms.at[ID,'sd']= np.std(measurement_data[meas].measurement)
+                ms.at[ID,'mean'] = np.mean(measurement_data[meas].measurement)
+                ms.at[ID,'sd'] = np.std(measurement_data[meas].measurement)
 
             print(ms)
 
@@ -86,7 +87,7 @@ for i_plotId, var_plotId in enumerate(plotIds):
                          label=visualization_specification[ind_plot].legendEntry[i]
                          )
             plt.legend()
-        elif indepVar=='time':
+        elif indepVar == 'time':
             plt.plot()
     plt.xlabel(visualization_specification.independentVariableName[i_plotId])
     plt.show()
